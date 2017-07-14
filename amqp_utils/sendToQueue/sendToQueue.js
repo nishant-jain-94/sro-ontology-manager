ce729fba37fdb615dec6598bbf83a6b1e3a93353
Nishant Jain <nishantkumarajain@gmail.com>
2017-07-14 12:54:51 +0530

    Added sendToQueue module in amqp_utils

---
+++ amqp_utils/sendToQueue/sendToQueue.js
@@ -0,0 +1,17 @@
+const log = require('../sro_utils/logger');
+const getAMQPChannel = require('../getAMQPChannel');
+
+const sendToQueue = ({message, queue, options={}}, cb) => {
+        const amqpConfigKey = (undefined === typeof options.amqpConfigKey) ? options.amqpConfigKey : 'AMQP_URL';
+        if(undefined !== typeof mappedQueue) {
+            getAMQPChannel(amqpConfigKey, (err, channel) => {
+                log.debug(message, queue);
+                channel.assertQueue(queue, {durable: true});
+                channel.sendToQueue(queue, new Buffer(JSON.stringify(message)));
+                log.info(`Message sent to ${queue}`);
+                if (cb) cb();
+            });
+        };
+};
+
+module.exports = sendToQueue;
