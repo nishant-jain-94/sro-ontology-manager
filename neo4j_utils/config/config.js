0f38085f9cd507cc47d3edbc37b3f8fcc604b129
Nishant Jain <nishantkumarajain@gmail.com>
2017-07-15 21:01:16 +0530

    Updated order of config details in neo4j/config.js

--- neo4j_utils/config/config.js
+++ neo4j_utils/config/config.js
@@ -1,11 +1,11 @@
 const neo4j_config = {
-NEO4J_HOST: 'localhost' || process.env.NEO4J_HOST,
-    NEO4J_PORT: 7474 || process.env.NEO4J_PORT,
-    NEO4J_BOLT_PORT: 7687 || process.env.NEO4J_BOLT_PORT,
-    NEO4J_PROTOCOL: 'bolt' || process.env.NEO4J_PROTOCOL,
-    NEO4J_USERNAME: 'neo4j' || process.env.NEO4J_USERNAME,
-    NEO4J_PASSWORD: 'password' || process.env.NEO4J_PASSWORD,
-    NEO4J_BASE64_ENCODED_CREDENTIALS: 'bmVvNGo6cGFzc3dvcmQ=' || process.env.NEO4J_BASE64_ENCODED_CREDENTIALS
+NEO4J_HOST: process.env.NEO4J_HOST || 'localhost',
+    NEO4J_PORT: process.env.NEO4J_PORT || 7474,
+    NEO4J_BOLT_PORT: process.env.NEO4J_BOLT_PORT || 7687,
+    NEO4J_PROTOCOL: process.env.NEO4J_PROTOCOL || 'bolt',
+    NEO4J_USERNAME: process.env.NEO4J_USERNAME || 'neo4j',
+    NEO4J_PASSWORD: process.env.NEO4J_PASSWORD || 'password',
+    NEO4J_BASE64_ENCODED_CREDENTIALS: process.env.NEO4J_BASE64_ENCODED_CREDENTIALS || 'bmVvNGo6cGFzc3dvcmQ=' 
 };
 
 module.exports = neo4j_config;
