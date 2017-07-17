0d65d06488842fead973ed413f4eb60774287d0c
Nishant Jain <nishantkumarajain@gmail.com>
2017-07-18 00:26:34 +0530

    Updated symlinks and createSymlinks.sh

--- scripts/createSymlinks.sh
+++ scripts/createSymlinks.sh
@@ -1,29 +1,15 @@
 #!/usr/bin/env bash
 
-:'
+sudo ln -sfn $PWD/sro_utils $PWD/amqp_utils/sro_utils
+sudo ln -sfn $PWD/sro_utils $PWD/ontology_broker/sro_utils
+sudo ln -sfn $PWD/amqp_utils $PWD/ontology_broker/amqp_utils
 
-The purpose of the createSymlinks.sh is to create a softlink for all the *_utils.
-The main intent is to share these modules across ontology_broker and ontology_processors.
-Advantages:
-1. Make it easier to create Docker containers
-2. Avoids long require paths
-
-'
-
-ln -sf $PWD/sro_utils $PWD/amqp_utils/sro_utils
-ln -sf $PWD/sro_utils $PWD/ontology_broker/sro_utils
-ln -sf $PWD/amqp_utils $PWD/ontology_broker/amqp_utils
-
-for processor in `find ./ontology_processors* -type d -maxdepth 1 \
-                    ! -path "./ontology_processors*/.nyc_output" \
-                    ! -path "./ontology_processors*/node_modules" \
-                    ! -path "./ontology_processors*/coverage" \
-                    ! -path "./ontology_processors*"`
+for processor in `find ./ontology_processors* -maxdepth 1 -type d ! -path "./ontology_processors*/.nyc_output" ! -path "./ontology_processors*/node_modules" ! -path "./ontology_processors*/coverage" ! -path "./ontology_processors" ! -path "./ontology_processors_neo4j"`
 
 do
-    ln -sf $PWD/sro_utils $PWD/$processor/sro_utils
-    ln -sf $PWD/neo4j_utils $PWD/$processor/neo4j_utils
-    ln -sf $PWD/amqp_utils $PWD/$processor/amqp_utils
+    sudo ln -sfn $PWD/sro_utils $PWD/$processor/sro_utils
+    sudo ln -sfn $PWD/neo4j_utils $PWD/$processor/neo4j_utils
+    sudo ln -sfn $PWD/amqp_utils $PWD/$processor/amqp_utils
 done
 
-sudo ln -sf $PWD/sro_utils $PWD/neo4j_utils
+sudo ln -sfn $PWD/sro_utils $PWD/neo4j_utils
