104382dc4792820203a66eebee9e2f5bb0ab2b49
Nishant Jain <nishantkumarajain@gmail.com>
2017-07-19 10:03:59 +0530

    Removed wait from the mongosetup.sh

--- scripts/mongosetup.sh
+++ scripts/mongosetup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 echo "Waiting for the standalone Replica Set"
-sleep 10
-
 MONGODB=`ping -c 1 mongodb | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
 
 until curl http://${MONGODB}:28017/serverStatus\?text\=1 2>&1 | grep uptime | head -1; do
@@ -24,5 +22,3 @@ cd /
 ls
 
 mongorestore -h ${MONGODB}:27017 /dump --noIndexRestore
-
-sleep 20
