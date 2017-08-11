8e16f0fbd9c3a47f3111dae4e1c78cf2cb4160da
Nishant Jain <nishantkumarajain@gmail.com>
2017-08-11 14:18:58 +0530

    Updated ontology_broker by creating the channels upfront

--- ontology_broker/mongo_utils/config/config.js
+++ ontology_broker/mongo_utils/config/config.js
@@ -1,5 +1,4 @@
 const config = {
     MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost'
 };
-console.log(process.env.MONGODB_URL);
 module.exports = config;
--- ontology_broker/queueMapper.js
+++ ontology_broker/queueMapper.js
@@ -5,10 +5,12 @@
 // Maps a Message of a certain type to the queue mapped. 
 const queueMapper = {
     'percp_scope_1.concepts': 'concept',
+    'percp_scope_1.course': 'course',
     'percp_scope_1.media_content': 'content',
     'percp_scope_1.user': 'user',
     'percp_scope_1.learner_state': 'learner_state',
-    'percp_scope_1.learning_resources': 'learning_resource'
+    'percp_scope_1.learning_resources': 'learning_resource',
+    'percp_scope_1.questions': 'questions'
 };
 
 // Exports the queueMapper Object.
