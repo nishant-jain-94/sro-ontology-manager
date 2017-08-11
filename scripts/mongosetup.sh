66908ac415b46653d3003519ebbaeb65675dba25
Nishant Jain <nishantkumarajain@gmail.com>
2017-08-11 14:34:23 +0530

    Updated mongosetup.sh to remove iputils and mongorestore command with getent and mongoimport

--- scripts/mongosetup.sh
+++ scripts/mongosetup.sh
@@ -2,17 +2,20 @@
 
 echo "Waiting for the standalone Replica Set"
 
-apt-get update
-apt-get install -y iputils-ping
+# apt-get update
+# apt-get install -y iputils-ping
 
-MONGODB=`ping -c 1 mongodb | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
+# ping google.com
 
-until curl http://${MONGODB}:28017/serverStatus\?text\=1 2>&1 | grep uptime | head -1; do
+MONGODB=`getent ahosts mongodb | awk 'NR==1 { print $1 }'`
+# MONGODB=`ping -c 1 mongodb | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
+
+until curl http://mongodb:28017/serverStatus\?text\=1 2>&1 | grep uptime | head -1; do
   printf '.'
   sleep 1
 done
 
-echo curl http://${MONGODB}:28017/serverStatus\?text\=1 2>&1 | grep uptime | head -1
+echo curl http://mongodb:28017/serverStatus\?text\=1 2>&1 | grep uptime | head -1
 echo "Started.."
 
 mongo --host ${MONGODB}:27017 <<EOF
@@ -20,6 +23,7 @@ mongo --host ${MONGODB}:27017 <<EOF
     rs.initiate()
 EOF
 cd /
-ls
 
-mongorestore -h ${MONGODB}:27017 /dump --noIndexRestore
+# mongorestore -h ${MONGODB}:27017 -d percp_scope_1 /dump_backup_1 --noIndexRestore
+# mongorestore -h ${MONGODB}:27017 -d percp_scope_1 /dump --noIndexRestore
+for file in $(ls /dump); do mongoimport -h ${MONGODB}:27017 -d percp_scope_1 /dump/$file; done
