82c04c59ce2fb6a81edfa5ca7e8ed324649e565d
Nishant Jain <nishantkumarajain@gmail.com>
2017-08-11 14:15:03 +0530

    Created channels upfront reading it from queueMapper.js

--- ontology_broker/index.js
+++ ontology_broker/index.js
@@ -9,26 +9,53 @@
 // 4. `getMongoDBConnection` returns a MongoDB Connection using `mongo_utils` module. 
 const async = require('async');
 const highland = require('highland');
-
+const _ = require('lodash');
 const log = require('./sro_utils/logger')('Ontology_Broker');
 const queueMapper = require('./queueMapper');
 const oplogHandler = require('./oplog_handler');
 const {getAMQPChannel} = require('./amqp_utils');
 const {getMongoDBConnection} = require('./mongo_utils');
 
+const createChannels = (cb) => {
+    const queues = _.uniq(_.values(queueMapper));
+    log.debug(queues);  
+    const amqpConfigKey = 'AMQP_URL';
+    getAMQPChannel(amqpConfigKey, (err, channel) => {
+        if (!err) {
+            channel.on('return', (msg) => {
+                log.debug("RETURN", msg);
+            });
+            const assertQueues = queues.map((queue) => { 
+                const assertQueue = (cb) => {
+                    channel.assertQueue(queue, {durable: true}, cb);
+                };
+               return assertQueue;
+            });
+            log.debug("Creating Channels");
+            async.series(assertQueues, cb);
+        } else {
+            cb(err, null);
+        }
+    }); 
+};
+
 // `toQueue` basically inputs an object containing `message`, `queue`, `options`(optional) and a `cb` 
 const toQueue = ({message, queue, options={}}, cb) => {
         const amqpConfigKey = (undefined === typeof options.amqpConfigKey) ? options.amqpConfigKey : 'AMQP_URL';
-        const mappedQueue = queueMapper[queue] ? queueMapper[queue]: 'junk';
-        if(undefined !== typeof mappedQueue) {
+        const mappedQueue = queueMapper[queue];
+        log.debug(!_.isUndefined(mappedQueue));
+        if(!_.isUndefined(mappedQueue)) {
             getAMQPChannel(amqpConfigKey, (err, channel) => {
                 log.debug(message, queue);
-                channel.assertQueue(mappedQueue, {durable: true});
-                channel.sendToQueue(mappedQueue, new Buffer(JSON.stringify(message)));
+                const status = channel.sendToQueue(mappedQueue, new Buffer(JSON.stringify(message), {}, (err, result) => {
+                    log.error(err);
+                    log.debug({result: result});
+                }));
+                log.debug('Status', status);
                 log.info(`Message sent to ${mappedQueue}`);
-                cb();
             });
         };
+        cb();
 };
 
 // `toQueueStream` is a callback wrapped version og `toQueue` which can easily be used in combination with highland streams. 
@@ -77,9 +104,9 @@ const streamOplogToOplogHandler = (oplogStream) => {
 // 2. `streamOplog` - Using the MongoDB connection object call the `streamOplog` to stream Oplog from MongoDB
 // 3. `streamOplogToOplogHandler` - Maps the stream to OplogHandler, which in return add's it to the queue
 async.waterfall([
-    getMongoDBConnection.bind(null, 'local'),
+    getMongoDBConnection.bind(null, 'local'), 
     streamOplog,
     streamOplogToOplogHandler
 ]);
 
-log.info("Tailing MongoDB Oplog");
+log.info("Tailing MongoDB Oplog");  
