3631a10e8b6f8858704dcefe11a22d62cc7e2ec6
Nishant Jain <nishantkumarajain@gmail.com>
2017-08-14 12:41:14 +0530

    Added installation scripts for question processor in install npm packages script

--- scripts/installNpmPackagesForAllComponents.sh
+++ scripts/installNpmPackagesForAllComponents.sh
@@ -19,5 +19,6 @@ cd ../../ontology_processors/percp_scope_1.media && npm install
 cd ../../ontology_processors/percp_scope_1.media_content && npm install
 cd ../../ontology_processors/percp_scope_1.user && npm install
 cd ../../ontology_processors/percp_scope_1.learning_resource && npm install
+cd ../../ontology_processors/percp_scope_1.questions && npm install
 cd ../../ontology_processors_api/ && npm install
 cd ../sro_utils/ && npm install
