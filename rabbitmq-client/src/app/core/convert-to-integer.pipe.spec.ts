69b3f0a2c2b21c48d5dd77bb472789cac7145b24
Ravi Chandran <chandrangreatfree@gmail.com>
2017-08-21 18:46:38 +0530

    Added sockets and pipes

---
+++ rabbitmq-client/src/app/core/convert-to-integer.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ConvertToIntegerPipe } from './convert-to-integer.pipe';
+
+describe('ConvertToIntegerPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ConvertToIntegerPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
