5ad84c1372758ef2e5a59146f045d3c047416fd9
Ravi Chandran <chandrangreatfree@gmail.com>
2017-08-23 15:59:14 +0530

    RabbitMQ Authentication credentials usage changes

--- rabbitmq-api-server/controller/index.js
+++ rabbitmq-api-server/controller/index.js
@@ -2,33 +2,35 @@ var request = require('request');
 
 var controller = {};
 
+var config = require('../config');
+
 controller.getNoOfQueues = (cb) => {
-   request.get('http://localhost:15672/api/queues', (err, response, body) => {
+   request.get('http://localhost:15672/api/queues', config.rabbitMQconfig,(err, response, body) => {
        if(!err && response.statusCode == 200) {
-        const data = {count: JSON.parse(body).length};
-        cb(null,  data);
+            const data = {count: JSON.parse(body).length};
+            cb(null,  data);
        } else {
-           cb(err, null);
+            cb(err, null);
        }
-   }).auth('guest', 'guest', false);
+   });
 };
 
 controller.getHealthStatus = (cb) => {
 
-    request.get('http://localhost:15672/api/healthchecks/node', function(err, response, body) {
+    request.get('http://localhost:15672/api/healthchecks/node', config.rabbitMQconfig,function(err, response, body) {
         if(!err && response.statusCode == 200) {
             const data = JSON.parse(body);
             cb(null, data);
         } else {
             cb(err, null);
         }
-    }).auth('guest','guest',false);
+    });
 
 };
 
 controller.getNoOfConsumers = (cb) => {
 
-    request.get('http://localhost:15672/api/consumers', function(err, response, body) {
+    request.get('http://localhost:15672/api/consumers', config.rabbitMQconfig,function(err, response, body) {
         if(!err && response.statusCode == 200) {
             const data = { count: JSON.parse(body).length };
             cb(null, data);
@@ -36,21 +38,21 @@ controller.getNoOfConsumers = (cb) => {
             cb(err, null);
         }
 
-    }).auth('guest','guest', false);
+    });
 
 };
 
 controller.getConsumerUtilisation = (cb) => {
 
-    request.get('http://localhost:15672/api/queues', (err, response, body) => {
+    request.get('http://localhost:15672/api/queues', config.rabbitMQconfig,(err, response, body) => {
         if(!err && response.statusCode == 200) {
-         const data = JSON.parse(body);
-         cb(null,  data);
+            const data = JSON.parse(body);
+            cb(null,  data);
         } else {
             cb(err, null);
         }
         
-    }).auth('guest', 'guest', false);
+    });
 
 }
 
