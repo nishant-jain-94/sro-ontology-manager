ec6fffe872b39f942efbcee25094096c7a7399cf
Nishant Jain <nishantkumarajain@gmail.com>
2017-08-24 14:20:01 +0530

    Updated createSymlinks to remove unnecessary symlinks being created in subfolders of ontology_processors_api

--- scripts/createSymlinks.sh
+++ scripts/createSymlinks.sh
@@ -4,7 +4,7 @@ sudo ln -sfn $PWD/sro_utils $PWD/amqp_utils/sro_utils
 sudo ln -sfn $PWD/sro_utils $PWD/ontology_broker/sro_utils
 sudo ln -sfn $PWD/amqp_utils $PWD/ontology_broker/amqp_utils
 
-for processor in `find ./ontology_processors* -maxdepth 1 -type d ! -path "./ontology_processors*/.nyc_output" ! -path "./ontology_processors*/node_modules" ! -path "./ontology_processors*/coverage" ! -path "./ontology_processors" ! -path "./ontology_processors_neo4j"`
+for processor in `find ./ontology_processors* -maxdepth 1 -type d ! -path "./ontology_processors*/.nyc_output" ! -path "./ontology_processors*/node_modules" ! -path "./ontology_processors*/coverage" ! -path "./ontology_processors" ! -path "./ontology_processors_neo4j" ! -path "./ontology_processors_api"`
 
 do
     sudo ln -sfn $PWD/sro_utils $PWD/$processor/sro_utils
@@ -13,3 +13,7 @@ do
 done
 
 sudo ln -sfn $PWD/sro_utils $PWD/neo4j_utils
+
+sudo ln -sfn $PWD/sro_utils $PWD/ontology_processors_api/sro_utils
+sudo ln -sfn $PWD/neo4j_utils $PWD/ontology_processors_api/neo4j_utils
+sudo ln -sfn $PWD/amqp_utils $PWD/ontology_processors_api/amqp_utils
