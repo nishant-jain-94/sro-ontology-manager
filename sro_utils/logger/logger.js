7d1d1f75391be80fda3331d0f9de886cf2a36964
Nishant Jain <nishantkumarajain@gmail.com>
2017-08-29 16:46:36 +0530

    Added Logging Mechanism using ELK Stack

--- sro_utils/logger/logger.js
+++ sro_utils/logger/logger.js
@@ -1,10 +1,8 @@
 const bunyan = require('bunyan');
 const bformat = require('bunyan-format');
-const formatOut = bformat({outputMode: 'short', levelInString: true});
 
 const logger = (moduleName) => bunyan.createLogger({
     name: moduleName,
-    stream: formatOut,
     level: 'debug'
 });
 
